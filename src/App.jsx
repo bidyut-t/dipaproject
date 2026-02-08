ba2b3876baeec5699a5df6b137165f5c10523a4d
bidyut-t <bidyut3004@gmail.com>
2026-02-08 11:50:05 +0530

    add: add login, signup and forgot password page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
-import { useState } from 'react'
-import Login from './pages/login/login';
-import Deshboard from './pages/deskboard';
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SignInPage from "./pages/auth/login/login";
+import SignUpPage from "./pages/auth/signup";
+import Deshboard from "./pages/deskboard";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <Deshboard/>
+      {/* <Login /> */}
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<SignInPage />} />
+          <Route path="/signin" element={<SignInPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          {/* <Route path="/forgot-password" element={<ForgotPasswordPage />} /> */}
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
